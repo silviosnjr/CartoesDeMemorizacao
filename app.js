b4c85d79a62652cf0438662ef6c5d87f79adf9c5
Silvio Sales do Nascimento Junior <silviosnjr@gmail.com>
2024-11-04 12:35:41 +0000

    código fonte da reunião 07

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function criarCartao(categoria, pergunta, resposta){
+function criarCartao(categoria, pergunta, resposta){    
     let cartao = document.createElement('article')
     cartao.className = 'cartao'
     cartao.innerHTML = `
@@ -12,6 +12,14 @@ function criarCartao(categoria, pergunta, resposta){
                     </div>
                 </div>
     `
+    let respostaEstaVisivel = false
+
+    function viraCartao(){
+        respostaEstaVisivel = !respostaEstaVisivel
+        cartao.classList.toggle('active', respostaEstaVisivel);
+    }
+    cartao.addEventListener('click', viraCartao)
+    
     let container = document.getElementById("container")
     container.appendChild(cartao)
-}
+}   
--- perguntas.js
+++ perguntas.js
@@ -9,3 +9,15 @@ criarCartao(
     "Qual a capital da França ?",
     "A capital da França é Paris"
 )
+
+criarCartao(
+    "Programação",
+    "O que é a função toggle no JavaScript ?",
+    "Essa função permite alternar entre dois estados, ativo ou inativo ou se existe ou não."
+)
+
+criarCartao(
+    "Programação",
+    "O que é HTML ?",
+    "É uma linguagem utilizada para marcar conteúdos e estruturas de uma página HTML."
+)
